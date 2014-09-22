eee8095e97f8ea747f6133b69cf52345b3a44ceb
Ronald Hernandez <2ronald.hernandez@gmail.com>
2014-09-22 13:21:23 -0400

    added the contact page and also click desk

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -10,8 +10,7 @@ validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{
 validates_length_of :content, :maximum => 500
 def update_spreadsheet
 connection = GoogleDrive.login(Rails.application.secrets.email_provider_username,
-Rails.application.secrets.email_provider_password
-)
+Rails.application.secrets.email_provider_password)
 ss = connection.spreadsheet_by_title('Learn-Rails-Example')
 if ss.nil?
 ss = connection.create_spreadsheet('Learn-Rails-Example')
